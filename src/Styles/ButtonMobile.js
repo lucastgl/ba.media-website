7d1d1c17f7e54ebdc46fcd742131ff6c9ccd0c73
ferreirabelen <ferreirabelenn@gmail.com>
2023-08-15 17:16:39 -0300

    prueba

--- src/Styles/ButtonMobile.js
+++ src/Styles/ButtonMobile.js
@@ -11,6 +11,7 @@ export const Button = styled.button`
     margin-right:20px;
     margin-bottom:25px;
     z-index:90;
+    //background-color:red;
     @media (min-width:750px){ 
         display:none;
     }
